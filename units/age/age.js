3d9c17439e303489526bebefb25dc03e546e9a6e
Aakas-h <76067278+Aakas-h@users.noreply.github.com>
2021-04-22 17:27:35 +0530

    unit part done

--- units/age/age.js
+++ units/age/age.js
@@ -6,26 +6,27 @@ today.value = d.getFullYear() + "-" + ((d.getMonth() + 1 < 10) ? "0" + parseInt(
 
 function compute() {
 
-    birthdate = document.getElementById("number2").value;
+    birthdate = document.getElementById("number1").value;
     today = document.getElementById("number2").value;
 
 
 
-    var date = new Date(birthdate.substring(0,4),birthdate.substring(5,7),birthdate.substring(8,10)).getMilliseconds();
-    var date2 = new Date(today.substring(0,4),today.substring(6,7),today.substring(9,10));
+    var date = new Date(birthdate.substring(0,4),birthdate.substring(5,7)-1,birthdate.substring(8,10),0,0,0,0);
+    var date2 = new Date(today.substring(0,4),today.substring(6,7)-1,today.substring(8,10),0,0,0,0);
 
 
+    
 
-    var diffrence = date2 - date;
-
-    console.log(date2.getFullYear());
+    var diffrence = date2.getTime() - date.getTime()
+    
     var newDate = new Date(diffrence);
+    
 
 
 
 
-    // document.getElementById("number3").value = todayyear - birthyear;
-    // document.getElementById("number4").value = todaymonth - birthmonth;
-    // document.getElementById("number5").value = 
+    document.getElementById("number3").value = newDate.getFullYear() - 1970;
+    document.getElementById("number4").value = newDate.getMonth();
+    document.getElementById("number5").value = newDate.getDate()-1;
 
 }
